fb8d668ae6154911d6493e97c5817307739b68f5
Fujita Hiroki <your.email@tafuji0405@gmail.com>
2020-03-03 18:54:57 +0000

    サインアップ　サインアップテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root    'static_pages#home'
   get     '/about',   to: 'static_pages#about'
   #get    '/login'
   #post   '/login'
   #delete '/logout'
-  # get     '/signup',   to: 'users#new'
-  get     'users/new'
+  get     '/signup',  to: 'users#new'
+  post    '/signup',  to: 'users#create'
+  resources :users
 end
