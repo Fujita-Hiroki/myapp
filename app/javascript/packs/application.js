aa22fcf0310287d3c6a7f9dd52fa95573d975b71
Hiroki Fujita <tafuji0405@gmail.com>
2020-03-22 13:09:27 +0000

    編集ページ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require("@rails/ujs").start()
-//= require("turbolinks").start()
-//= require("@rails/activestorage").start()
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
 //= require("channels")
 //= require('jquery');
 
-import 'bootstrap'
-import '@fortawesome/fontawesome-free/js/all'
-import 'src/stylesheets/application.scss';
-import 'src/application';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all';
+import 'src/stylesheets/application';
